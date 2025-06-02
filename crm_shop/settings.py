b4421ea45b6a185fa3c1d7f1d9ec354418049e3c
Diyorbek Xakimov <diyorbekxakimov@GALLAXY-S30-ULTRA.local>
2025-06-03 02:47:27 +0500

    Fix: final flake8 errors

--- crm_shop/settings.py
+++ crm_shop/settings.py
@@ -146,9 +146,7 @@ if not DEBUG:
     EMAIL_USE_TLS = True
     EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
     EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
-    DEFAULT_FROM_EMAIL = os.environ.get(
-        "DEFAULT_FROM_EMAIL", "noreply@fashionstore.com"
-    )
+    DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL", "noreply@fashionstore.com")
 
 # Logging configuration
 LOGGING = {
