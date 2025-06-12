0dae4e3e4ba15fa75f6f0695e120136707a9d112
diyorbekxakimov <dajtedajte3@gmail.com>
2025-06-12 19:57:39 +0500

    ci/cd

---
+++ settings/__init__.py
@@ -0,0 +1,3 @@
+# crm_shop/settings/__init__.py
+
+# This file makes the settings folder a Python package.
---
+++ settings/production.py
@@ -0,0 +1,7 @@
+from .base import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['your-app-name.onrender.com']
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
