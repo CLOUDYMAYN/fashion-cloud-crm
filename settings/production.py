0811b4e0c16db3f4c340d73998406987d8aecae5
diyorbekxakimov <dajtedajte3@gmail.com>
2025-06-12 22:55:11 +0500

    fix: добавлен gunicorn для Render

--- settings/production.py
+++ settings/production.py
@@ -1,7 +1,7 @@
 # from .base import *
-#
-# DEBUG = False
-#
-# ALLOWED_HOSTS = ['your-app-name.onrender.com']
-#
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['https://fashion-cloud-crm-1.onrender.com']
+
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
