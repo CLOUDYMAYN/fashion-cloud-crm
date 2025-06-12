4233f39781b88ae560a53fbde4fdb271dfd3c5f6
diyorbekxakimov <dajtedajte3@gmail.com>
2025-06-13 00:39:37 +0500

    fix: added staticfiles collect

--- settings/production.py
+++ settings/production.py
@@ -1,11 +1,16 @@
+import os
+from pathlib import Path
 from .base import *
 
 DEBUG = False
 
 ALLOWED_HOSTS = ['fashion-cloud-crm-1.onrender.com']
 
-# Поддержка базы данных через URL:
 import dj_database_url
 DATABASES = {
     "default": dj_database_url.config(default=os.getenv("DATABASE_URL"))
 }
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
